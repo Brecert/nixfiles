dc691a6f45cc2d2055751444c72a15296b2b2930
brecert <me@bree.dev>
2021-09-28 07:19:12 -0400

    update inputs with fix for mach0

--- sylv/configuration.nix
+++ sylv/configuration.nix
@@ -150,33 +150,6 @@
     '';
   };
 
-  nixpkgs.overlays = [
-    (self: super: {
-      nixUnstable = super.nixUnstable.override {
-        patches = [
-          (pkgs.writeTextFile {
-            name = "unset-is-macho.patch";
-            text = ''
-              diff --git a/src/nix/get-env.sh b/src/nix/get-env.sh
-              index 42c806450..a8563c772 100644
-              --- a/src/nix/get-env.sh
-              +++ b/src/nix/get-env.sh
-              @@ -8,6 +8,8 @@ if [[ -n $stdenv ]]; then
-                   source $stdenv/setup
-               fi
-               
-              +unset -f isMachO
-              +
-               # Better to use compgen, but stdenv bash doesn't have it.
-               __vars="$(declare -p)"
-               __functions="$(declare -F)"
-            '';
-          })
-        ];
-      };
-    })
-  ];
-
   # Programs and Services configuration
   environment.systemPackages = with pkgs; [
     micro
