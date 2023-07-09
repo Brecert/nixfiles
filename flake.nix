bd341a07450bf21ee935f893413056a864a65344
brecert <me@bree.dev>
2023-07-09 00:17:47 -0400

    update inputs

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     fenix = {
       url = "github:nix-community/fenix";
--- nymi/bree/packages.nix
+++ nymi/bree/packages.nix
@@ -1,31 +1,13 @@
 { config, lib, pkgs, packages, ... }:
 
-let
-  imhex-up = pkgs.imhex.overrideAttrs (final: prev: rec {
-    version = "1.29.0";
-
-    src = pkgs.fetchFromGitHub {
-      fetchSubmodules = true;
-      owner = "WerWolv";
-      repo = prev.pname;
-      rev = "v${version}";
-      hash = "sha256-dghyv7rpqGs5dt51ziAaeb/Ba7rGEcJ54AYKRJ2xXuk=";
-    };
-
-    cmakeFlags = prev.cmakeFlags ++ [
-      "DCREATE_PACKAGE=ON"
-    ];
-  });
-in
-
 {
   home.packages =
-    [ imhex-up ]
-    ++ (with pkgs; [
+    (with pkgs; [
       # Applications
       tdesktop
       prismlauncher
       sublime-merge
+      imhex
 
       # Nix Utils
       nil
